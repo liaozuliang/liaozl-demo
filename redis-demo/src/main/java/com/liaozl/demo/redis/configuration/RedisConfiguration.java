04797a9768a1eab1c4bb3ad15d9a36badcf212bd
liaozuliang <liaozuliang@zywamail.com>
2020-08-14 12:29:15 +0800

    解决没有启用redis连接池问题

--- redis-demo/src/main/java/com/liaozl/demo/redis/configuration/RedisConfiguration.java
+++ redis-demo/src/main/java/com/liaozl/demo/redis/configuration/RedisConfiguration.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,6 +22,11 @@ public class RedisConfiguration {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        if (factory instanceof LettuceConnectionFactory) {
+            LettuceConnectionFactory lettuceConnectionFactory = (LettuceConnectionFactory) factory;
+            lettuceConnectionFactory.setShareNativeConnection(false);
+        }
+
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(factory);
 
